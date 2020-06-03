5205bc231a84e9a73b6d8b156823b033c2fe565b
Kristel <kguzmanv@ucenfotec.ac.cr>
2020-06-03 13:32:39 -0600

    Se eliminan documentos

--- Ruby/Histograma.rb
+++
@@ -1,4 +0,0 @@
-#Autor Kristel Guzman Venegas
-
-myArray = [1,2,1,3,3,1,2,1,5,1]
-(1..5).each { |i| puts "#{i}: " + '*' * myArray.count(i)  }
--- Ruby/NumeroMasAlto.rb
+++
@@ -1,4 +0,0 @@
-#Autor Kristel Guzman Venegas
-
-myarray = [13,2,4,35,1];
-puts myarray.max
